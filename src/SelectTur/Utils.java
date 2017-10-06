8925bb1876e622bc9651dde7b20e205da1bee634
Paul Leger <pleger@gmail.com>
2017-10-06 16:03:28 -0300

    Adding a library

--- src/SelectTur/Utils.java
+++ src/SelectTur/Utils.java
@@ -1,13 +1,21 @@
 package SelectTur;
 
 import java.util.Random;
+import org.apache.commons.math3.distribution.NormalDistribution;
+
 
 public class Utils {
 
+    public static NormalDistribution ndp = new NormalDistribution(100.87,67.16);
     public static Random random = new Random();
 
     public static double getTolerancia() {
         return 0.2 + random.nextDouble() * (0.28 - 0.2); //u = 0.25
+    }
+
+    public static double getPresupuesto() {
+        return ndp.sample();
+
     }
 
 

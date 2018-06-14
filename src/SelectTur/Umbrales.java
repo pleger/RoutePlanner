b6c10137f169591f6f587e1dafe83039e19ed666
Vicky Pizarro <vickypizarro@outlook.com>
2018-06-14 16:06:09 -0400

    pandora

--- src/SelectTur/Umbrales.java
+++ src/SelectTur/Umbrales.java
@@ -2,26 +2,28 @@ package SelectTur;
 
 public class Umbrales {
 
-    static final double[][] UMBRALESSATISFACCIONES = new double[][]{
-            {0,0,11.5962261555677},
-            {1,0,17.9406656869605},
-            {2,0,24.2851052183534},
-            {0,1,7.1924986964064},
-            {1,1,11.1867595608615},
-            {2,1,15.1810204253166},
-            {0,2,8.93105537263441},
-            {1,2,13.9677851944196},
-            {2,2,19.0045150162048},
-            {0,3,8.55071536845512},
-            {1,3,13.6904208713611},
-            {2,3,18.8301263742671},
-            {0,4,3.83591242935517},
-            {1,4,6.31436378538473},
-            {2,4,8.79281514141428},
-            {0,5,5.69758301947929},
-            {1,5,9.91188892137122},
-            {2,5,14.1261948232631},
-            {0,6,3.78083133328989},
-            {1,6,5.84787560126427},
-            {2,6,7.91491986923864},};
+    private static void crearUmbralProvincias() {
+        umbralProvincia = new double[TIPOUMBRAL][TIPOTURISTA];
+        umbralProvincia[0][0] = 11.5962261555677;
+        umbralProvincia[1][0] = 17.9406656869605;
+        umbralProvincia[2][0] = 24.2851052183534;
+        umbralProvincia[0][1] = 7.1924986964064;
+        umbralProvincia[1][1] = 11.1867595608615;
+        umbralProvincia[2][1] = 15.1810204253166;
+        umbralProvincia[0][2] = 8.93105537263441;
+        umbralProvincia[1][2] = 13.9677851944196;
+        umbralProvincia[2][2] = 19.0045150162048;
+        umbralProvincia[0][3] = 8.55071536845512;
+        umbralProvincia[1][3] = 13.6904208713611;
+        umbralProvincia[2][3] = 18.8301263742671;
+        umbralProvincia[0][4] = 3.83591242935517;
+        umbralProvincia[1][4] = 6.31436378538473;
+        umbralProvincia[2][4] = 8.79281514141428;
+        umbralProvincia[0][5] = 5.69758301947929;
+        umbralProvincia[1][5] = 9.91188892137122;
+        umbralProvincia[2][5] = 14.1261948232631;
+        umbralProvincia[0][6] = 3.78083133328989;
+        umbralProvincia[1][6] = 5.84787560126427;
+        umbralProvincia[2][6] = 7.91491986923864;
+    }
 }
